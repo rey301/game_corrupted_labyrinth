4d9e42aca7e4c1bd2eae6e5e63dc16a637cd659c
rey301 <abrahamrey3001@gmail.com>
2025-12-08 20:03:05 +0000

    Update Monster class dependencies.

--- monster.py
+++ monster.py
@@ -1,3 +1,7 @@
+from item import Item
+from character import Character
+
 class Monster:
-    def __init__(self, reward):
+    def __init__(self, reward, hp, max_hp, attack_power):
+        super.__init__(hp, max_hp, attack_power)
         self.reward = reward
