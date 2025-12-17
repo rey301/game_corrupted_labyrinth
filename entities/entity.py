ffe7d118bba070e890311e78f26261de869896ae
rey301 <abrahamrey3001@gmail.com>
2025-12-17 18:17:17 +0000

    Change file locations.

--- entities/entity.py
+++
@@ -1,8 +0,0 @@
-class Entity:
-    """
-    An entity is any object that is in the game world where they have a name and a description.
-    """
-    def __init__(self, name, description=None):
-        self.name = name
-        self.description = description
-
